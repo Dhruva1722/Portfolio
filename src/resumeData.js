5a9ea0d9fa167c0ab12b4269b88a791ff5251683
Dhruva1722 <dhruvasolanki17@gmail.com>
2023-06-06 15:07:17 +0530

    first commit

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,64 +1,67 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "name": "Dhruva Solanki",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/dhruva17/",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, I also have a growing interest in Android app development using Java. I enjoy creating user-friendly interfaces.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/dhruva17/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/Dhruva1722",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
+          "name":"instagram",
+          "url":"http://instagram.com/dhruva_.17",
+          "className":"fa fa-instagram"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "aboutme":"I am currently doing an internship at The Multispan Control Instruments Pvt Ltd. I am a self taught Full Stack Web Developer, currently diving deeper into Frontend Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
     "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "website":"https://dhruva1722.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"N.V.Patel School",
+        "specialization":"H.S.C",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        // "Achievements":"Some Achievements"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"LJ Collage of Computer Application",
+        "specialization":"B.C.A",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        // "Achievements":"Some Achievements"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
+        "CompanyName":"Multispan Control Instruments Pvt Ltd",
+        "specialization":"Full Stack Developer",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2023",
         "Achievements":"Some Achievements"
       },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
     ],
     "skillsDescription":"Your skills here",
     "skills":[
+      {
+        "skillname":"Android Studio"
+      },
       {
         "skillname":"HTML5"
       },
@@ -71,24 +74,24 @@ let resumeData = {
     ],
     "portfolio":[
       {
-        "name":"project1",
+        "name":"Rental hub",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"assets/rental.jpg"
       },
       {
         "name":"project2",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "imgurl":"assets/multispan.jpg"
       },
       {
-        "name":"project3",
+        "name":"NewsFTP",
         "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "imgurl":"assets/news.jpg"
       },
       {
-        "name":"project4",
+        "name":"HostelLife",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"assets/hostel.jpg"
       }
     ],
     "testimonials":[
